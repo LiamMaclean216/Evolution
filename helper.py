eff1501511e7b7a1de9ba0d11bff7bb79a257b49
Liam <lmaclean234@gmail.com>
2018-09-29 18:22:36 -0400

    bottemless GAN loss, normalized fitness

--- helper.py
+++ helper.py
@@ -83,7 +83,7 @@ def measure_fitness(creature,env,device,discrete_actions,render = False,max_step
         
         observation, reward, done, _ = env.step(action)
         
-        total_reward += max(reward,0)
+        total_reward += reward
         
         if done:
             break
