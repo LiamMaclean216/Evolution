35daf0214999069c91c5b4fbc857021b54f74c8e
Liam <lmaclean234@gmail.com>
2018-10-07 10:16:40 -0400

    cleanup and comment

--- helper.py
+++ helper.py
@@ -15,23 +15,6 @@ def set_params(model,data):
         idx+=p.numel()
     return model
 
-def run_params(model,model_data,input):
-    idx = 0
-    out = input
-    for i,p in enumerate(model.parameters()):
-        view = model_data[idx:idx+p.numel()].view(p.size())
-    
-        
-        idx+=p.numel()
-        
-        
-        if i % 2 == 0:
-            out = torch.matmul(out, view.transpose(0,1))
-        else :
-            out += view
-            
-    return out
-
 #get model parameters as vector
 def get_params(model):
     params = []
@@ -41,21 +24,25 @@ def get_params(model):
     params = torch.cat(params, dim=0)
     return params
 
-
+#get probability of creature getting picked based on fitness
+def get_pick_probabilities(p_fitness):
+    if(np.min(p_fitness) == np.max(p_fitness)):
+        return np.ones(p_fitness.shape)/p_fitness.size
+    
+    pick_probabilities = (p_fitness-np.min(p_fitness))
+    pick_probabilities = pick_probabilities/np.sum(pick_probabilities)
+    return pick_probabilities
 
 
 #measure creature fitness
-def measure_fitness(creature,env,device,discrete_actions,render = False,max_steps = 1000,n_behavior_samples = 10):
+def measure_fitness(creature,env,device,discrete_actions,render = False,max_steps = 1000):
     observation = env.reset()
     #creature fitness is cumulative reward in simulation
     total_reward = 0
     
-    #sample behavior from episode for autoencoder training
-    behavior_samples = []
     for i in range(max_steps):
         
         if render:
-            
             env.render()
             
         #convert observation into tensor
@@ -71,10 +58,6 @@ def measure_fitness(creature,env,device,discrete_actions,render = False,max_step
             sample = (obs,action)
             action = action.detach().cpu().numpy()
             
-        #add current sample to all behavior samples
-        
-        behavior_samples.append(sample)
-        
         observation, reward, done, _ = env.step(action)
         
         total_reward += reward
@@ -82,22 +65,13 @@ def measure_fitness(creature,env,device,discrete_actions,render = False,max_step
         if done:
             break
     
-    #reshape behavior samples into tensors
-    behavior_samples = random.sample(behavior_samples,min(n_behavior_samples,len(behavior_samples)))
-    act,obs = zip(*behavior_samples)
-    return total_reward, torch.stack(act,0), torch.stack(obs,0)
+    return total_reward
 
 #measure fitness of entire population and return scores
-def measure_population_fitness(population,env,device,discrete_actions,max_steps = 1000,n_behavior_samples = 10):
+def measure_population_fitness(population,env,device,discrete_actions,max_steps = 1000):
     scores = []
-    actions = []
-    observations = []
-    for idx,p in enumerate(population):
-       #print("measuring fitness : {}".format(idx))
-        fitness, act,obs = measure_fitness(p,env,device,discrete_actions,max_steps = max_steps,n_behavior_samples=n_behavior_samples)
-        scores.append(fitness)
-        
-        actions.append(act)
-        observations.append(obs)
 
-    return np.array(scores),(actions,observations)
+    for idx,p in enumerate(population):
+        fitness = measure_fitness(p,env,device,discrete_actions,max_steps = max_steps)
+        scores.append(fitness)
+    return np.array(scores)
